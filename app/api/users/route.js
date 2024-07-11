69bd786eb646e8a01001fe11f1b9de4745c517a3
Haris <harisgujjar317@gmail.com>
2024-07-11 12:26:45 -0400

    advance url

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,6 +3,13 @@ import { users } from '@/lib/db.js'
 
 
 // advance routing url params
-export async function GET() {
-    return NextResponse.json(users)
+export async function GET(request) {
+    const { searchParams } = new URL(request.url)
+    const name = searchParams.get('name');
+    const age = searchParams.get('age');
+    console.log(request.url)
+    console.log(name);
+    console.log(age);
+    const obj = Object.fromEntries(searchParams.entries())
+    return NextResponse.json(obj)
 }
